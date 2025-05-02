316d324ea4bd9521788cda5405f32f2a23b7d484
crsa-um6p <ayoub.bouihrouchane@um6p.ma>
2025-05-02 17:30:05 +0100

    add Puit model

--- backend/visualisation/models.py
+++ backend/visualisation/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+
+class Puit(models.Model):
+    geometry = models.PointField()
+    date = models.DateField()
+
+    
+
