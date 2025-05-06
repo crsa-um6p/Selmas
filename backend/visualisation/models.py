597de8709fd370934ef9a3ca11de6aa3bc3445ff
crsa-um6p <ayoub.bouihrouchane@um6p.ma>
2025-05-06 14:35:02 +0100

    add sample model

--- backend/visualisation/models.py
+++ backend/visualisation/models.py
@@ -9,3 +9,19 @@ class Puit(models.Model):
 
     
 
+
+class Sample(models.Model):
+    code_labo = models.CharField(primary_key=True)
+    reference = models.CharField(max_length=255)
+    nature = models.CharField(max_length=255)
+    geometry = models.PointField()
+    date_collecte = models.DateField()
+    date_reception = models.DateField()
+    date_edition = models.DateField()
+
+
+class Texture(models.Model):
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+    
+
+
