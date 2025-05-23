c0b9d4e1fb2c18d21016e5098a21e09b65d86ad2
crsa-um6p <ayoub.bouihrouchane@um6p.ma>
2025-05-23 17:25:35 +0100

    add a new utl to get data

--- backend/visualisation/urls.py
+++ backend/visualisation/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("add_puit/", views.add_puit, name="add_puit"),
+    path("get_data/", views.get_data, name="get_data"),
 ]
 
