1b1e15b3956d6c1507f4f804bfee5b465c27a536
crsa-um6p <ayoub.bouihrouchane@um6p.ma>
2025-05-23 17:25:08 +0100

    add view function to get data

--- backend/visualisation/views.py
+++ backend/visualisation/views.py
@@ -1,21 +1,47 @@
 from django.shortcuts import render
-
-##from .models import Puit
-##from django.contrib.gis.geos import Point
-##from datetime import date
-
-###from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+import pandas
+import json
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+@csrf_exempt
+def get_data(request):
+    parameters = json.loads(request.body)
+    print("--------------------------------")
+    print(parameters)
+    print("--------------------------------")
+    min_value = parameters['min_value']
+    max_value = parameters['max_value']
+    variable = parameters['variable']
+    filter = parameters['filter']
 
-# def add_puit(request):
+    if variable == "soiltexture":
+        if filter == "Argile":
+            data = soilTexture.objects.filter(Argile__range=(min_value, max_value))
+            
+        elif filter == "Lemon":
+            data = soilTexture.objects.filter(Lemon__range=(min_value, max_value))
+        elif filter == "Sable":
+            data = soilTexture.objects.filter(Sable__range=(min_value, max_value))
+        elif filter == "Soil_texture_v4":
+            data = soilTexture.objects.filter(Soil_texture_v4__range=(min_value, max_value))
+        data_list = list(data.values("Code_labo_id", "Argile", "Lemon", "Sable", "Soil_texture_v4"))
+    
+    elif variable == "soilsample":
+        if filter == "date":
+            data = soilSample.objects.filter(Date_edition__range=(min_value, max_value))
+        elif filter == "depth":
+            data = soilSample.objects.filter(Depth__range=(min_value, max_value))
+        elif filter == "code_labo":
+            data = soilSample.objects.filter(Code_labo__range=(min_value, max_value))
+        data_list = list(data.values("Code_labo", "Depth", "Date_edition"))
+    
 
-#     # Puit.objects.create(geometry=Point(1, 1), date=date.today())
+    
 
-#     data = pandas.read_excel("data.xlsx")
 
-#     for index, row in data.iterrows():
-#         Puit.objects.create(geometry=Point(row['x'], row['y']), date=row['date'])
+    
+    return JsonResponse(data_list, safe=False)
 
-#     return HttpResponse("Puits added")
